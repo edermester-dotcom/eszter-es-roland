d4098403265af6e05b341db5dfe6be8d8ea6a601
edermester-dotcom <edermester@gmail.com>
2026-07-05 16:18:56 +0200

    V2 main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
-function start(){alert("A következő jelenet itt fog kezdődni.");}
+const button = document.getElementById("startButton");
+
+button.addEventListener("click", () => {
+
+    document.body.style.transition = "opacity 2s";
+
+    document.body.style.opacity = "0";
+
+    setTimeout(() => {
+
+        alert("Itt kezdődik majd a történet következő fejezete.\n\n(Ezt a következő lépésben készítjük el.)");
+
+        document.body.style.opacity = "1";
+
+    }, 2000);
+
+});
